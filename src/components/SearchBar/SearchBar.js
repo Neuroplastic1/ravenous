f518745b2a2121246a89baf8f7381f6d519e82a5
Neuroplastic1 <shafqat.umair@gmail.com>
2021-10-15 11:15:16 -0400

    Moved info through props  Moved business information to the container component (App)  Passed information from parent components (App) to child components (BusinessList, Business)

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -18,20 +18,20 @@ class SearchBar extends React.Component{
 
   render() {
     return (
-      <div className="SearchBar">
-  <div className="SearchBar-sort-options">
-    <ul>
-      {this.renderSortByOptions()}
-    </ul>
-  </div>
-  <div className="SearchBar-fields">
-    <input placeholder="Search Businesses" />
-    <input placeholder="Where?" />
-  </div>
-  <div className="SearchBar-submit">
-    <a href="www.#.com">Let's Go</a>
-  </div>
-</div>
+    <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+           <ul>
+             {this.renderSortByOptions()}
+           </ul>
+        </div>
+      <div className="SearchBar-fields">
+        <input placeholder="Search Businesses" />
+        <input placeholder="Where?" />
+      </div>
+      <div className="SearchBar-submit">
+         <a href="www.#.com">Let's Go</a>
+      </div>
+    </div>
     );
   }
 }
