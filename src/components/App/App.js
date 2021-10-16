680f7255391153030d36c4765388f478265b2f1f
Neuroplastic1 <shafqat.umair@gmail.com>
2021-10-16 06:38:03 -0400

    Added visual feedback for the sorting options at the top of the search bar Set the state of sorting options and input elements Simulated a search query with the “Let’s Go” button Added handleChange functions to facilitate these features.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import BusinessList from './../BusinessList/BusinessList';
 import SearchBar from './../SearchBar/SearchBar';
@@ -17,14 +17,26 @@ const business = {
 
 const businesses = [business,business,business,business,business,business]
 
-function App() {
-  return (
-    <div className="App">
-     <h1>ravenous</h1>
-     <SearchBar />
-     <BusinessList businesses={businesses}/>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props) {
+        super(props)
 
-export default App;
+     this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp = (term, location, sortBy) => {
+      console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`)
+  }
+
+  render() {
+     return (
+        <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp}/>
+        <BusinessList businesses={businesses}/>
+        </div>
+       );
+     }
+ }
+
+export default App
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,18 +1,61 @@
-import React from 'react'
+import React, { Component } from 'react';
 import './SearchBar.css'
 
-const sortByOptions = {
-  'Best Match':'best-match',
-  'Highest Rated':'rating',
-  'Most Reviewed':'review_count'
+class SearchBar extends Component{
+  constructor(props) {
+    super(props);
+// initial states
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+    };
 
-}
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleLocationChange = this.handleLocationChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
 
-class SearchBar extends React.Component{
+    this.sortByOptions = {
+      'Best Match':'best-match',
+      'Highest Rated':'rating',
+      'Most Reviewed':'review_count'
+
+    }
+  }
+// returns the current CSS class for a sorting option
+  getSortByClass(sortByOption) {
+    if(this.state.sortBy === sortByOption) {
+      return 'active'
+    } else { return ''}
+  }
+// sets the state of a sorting option
+  handleSortByChange(sortByOption) {
+    this.setState({sortBy : sortByOption})
+  }
+// handleChagne for term input element
+  handleTermChange(e) {
+    this.setState({ term : e.target.value})
+  }
+// handleChange for location input element
+  handleLocationChange(e) {
+    this.setState({ location : e.target.location})
+  }
+
+  handleSearch(e) {
+    this.props.searchYelp(this.state.term, this.state.location, this.state.sortby);
+// prevent the default action of clicking a link from triggering at the end of the method
+    e.preventDefault()
+  }
+
   renderSortByOptions() {
-    return Object.keys(sortByOptions).map(sortByOption => {
-      let sortByOptionValue = sortByOptions[sortByOption];
-      return (<li key={sortByOptionValue}>{sortByOption}</li>)
+    return Object.keys(this.sortByOptions).map(sortByOption => {
+      let sortByOptionValue = this.sortByOptions[sortByOption];
+      return (<li
+                  key={sortByOptionValue}
+                  className={this.getSortByClass(sortByOptionValue)}
+                  onClick={this.handleSortByChange.bind(this, sortByOptionValue)}>
+                  {sortByOption}
+              </li>)
     });
   }
 
@@ -25,15 +68,15 @@ class SearchBar extends React.Component{
            </ul>
         </div>
       <div className="SearchBar-fields">
-        <input placeholder="Search Businesses" />
-        <input placeholder="Where?" />
+        <input placeholder="Search Businesses" onChange={this.handleTermChange}/>
+        <input placeholder="Where?" onChange={this.handleLocationChange}/>
       </div>
       <div className="SearchBar-submit">
-         <a href="www.#.com">Let's Go</a>
+         <a href="www.#.com" onClick={this.handleSearch}>Let's Go</a>
       </div>
     </div>
     );
   }
 }
 
-export default SearchBar;
+export default SearchBar
